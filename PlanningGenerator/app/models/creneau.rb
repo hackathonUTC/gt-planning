42d0c76ca927942e03151abd7ff23c18ddc95c91
Tanguy Le Floch <tanguy@Tanguy-G53SX.local.dev>
2015-11-28 21:09:27 +0100

    Formulaire 1 valide

--- PlanningGenerator/app/models/creneau.rb
+++ PlanningGenerator/app/models/creneau.rb
@@ -2,6 +2,7 @@ class Creneau < ActiveRecord::Base
 	belongs_to :event
 	has_many :participants, :through => :disponibilites
 	validates_associated :event
-	validates :date, presence: true
+	validates :date_fin, :presence => "true"
+	validates :date_debut, :presence => "true"
 	validates :Nombre_de_Participants_Necessaires, presence: true
 end
